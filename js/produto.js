6f4a8910f07f8dac58ae416b20c0539f516d6f0d
kalinekp <kaline-kp@hotmail.com>
2024-06-29 11:51:40 -0300

    FUNCOES ANONIMAS, MANUPULANDO STRINGS E BOTAO DE CALCULO DE COMPRA9 9.14

--- js/produto.js
+++ js/produto.js
@@ -1,4 +1,4 @@
-
+// FUNÇÃO DO BOTÃO 'SLIDE' DE TAMANHO DA ROUPA
 var inputTamanho = document.querySelector('[name=tamanho]')
 var outputTamanho = document.querySelector('[name=valortamanho]')
 
@@ -8,3 +8,31 @@ function mostraTamanho(){
 
 inputTamanho.oninput = mostraTamanho
 
+
+
+/* EXEMPLOS DE 'STRINGS COM OUTRO TIPO DE DADOS'
+
+var num1 = 2;
+var num2 = 3;
+var nome = "Caelum"
+
+console.log(num1 + nome + num2);
+
+console.log(num1 + num2 + nome);
+
+console.log(nome + num1 + num2);
+
+console.log(nome + (num1 + num2));
+
+console.log(nome + num1 * num2);
+
+--------------------------------------
+
+EXEMPLO DE 'NaN'
+
+var resultado = 10 -"curso"; //retorna NaN
+resultado == NaN; //false
+NaN == NaN; //false
+
+isNaN(resultado); //true
+*/
---
+++ js/total.js
@@ -0,0 +1,7 @@
+//BOTÃO DE CALCULAR A COMPRA || TAG <dd> vezes o valor que o usuario por no <input>
+var $input_quantidade = document.querySelector("#qtd");
+var $outout_total = document.querySelector("#total");
+
+$input_quantidade.oninput = function() {// FUNÇÃO ANONIMA QUE TAMBEM PODE SER FEITA ASSIM: $input_quantidade.oninput = () => {}
+
+}
