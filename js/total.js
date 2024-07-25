7eff1fef9f62b9a271a3a7ead34448ac21ceea54
kalinekp <kaline-kp@hotmail.com>
2024-07-25 16:20:37 -0300

    Função do input preço e total com JS

--- js/total.js
+++ js/total.js
@@ -3,5 +3,15 @@ var $input_quantidade = document.querySelector("#qtd");
 var $outout_total = document.querySelector("#total");
 
 $input_quantidade.oninput = function() {// FUNÇÃO ANONIMA QUE TAMBEM PODE SER FEITA ASSIM: $input_quantidade.oninput = () => {}
+var preco = document.querySelector("#preco").textContent;
+preco = preco.replace("R$ ", "");
+preco = preco.replace(",",".");
+preco = parseFloat(preco);
 
+var quantidade = $input_quantidade.value;
+var total = quantidade * preco;
+total = "R$" + total.toFixed(2)
+total = total.replace(".",",");
+
+$outout_total.value = total;
 }
