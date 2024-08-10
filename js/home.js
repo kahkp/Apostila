3d15732f4c79f570269e343cbfceb8359c14eb31
kalinekp <kaline-kp@hotmail.com>
2024-08-10 11:27:31 -0300

    Plugins jQuery e iframes

--- js/home.js
+++ js/home.js
@@ -1,3 +1,16 @@
-$('.novidades button').click(function () {
-  $('.novidades').addClass('painel-aberto');
+//BOTÃO MOSTRAR MAIS PARA OS DOIS PAINEIS
+//COM JQUERY
+/*Ele adiciona e remove(toggleClass)a classe do botão
+quando acionado*/
+$('.painel button').click(function () {
+  $(this).parent().toggleClass('painel-aberto');
+  $(this).text("Mostrar menos");
 });
+
+/*$('.mais-vendidos button').click(function () {
+  $('.mais-vendidos').toggleClass('painel-aberto');
+});                   (.addClass é só para adicionar)
+
+$('.novidades button').click(function () {
+  $('.novidades').toggleClass('painel-aberto');
+});               (toggleClass adiociona e remove)*/             
